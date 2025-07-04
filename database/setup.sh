300c38a2dc81a29a7932c4b53fcbdcefe84ac101
alejo <alejandro.vaz.myt@gmail.com>
2025-07-04 15:59:50 +0200

    updated templates & fixed db script

--- database/setup.sh
+++ database/setup.sh
@@ -3,10 +3,10 @@
 #
 
 # EXECUTE -> RESET DATABASE
-mysql -u root -p < /srv/www/abscissa/scripts/reset.sql
+mysql -u root -p < /srv/www/website/database/reset.sql
 
 # EXECUTE -> INJECT DATABASE
-mysql -u root -p abscissa < /srv/www/abscissa/public/db/database.sql
+mysql -u root -p abscissa < /srv/www/website/database/database.sql
 
 # EXECUTE -> DATABASE INITIAL STATE
-mysql -u root -p abscissa < /srv/www/abscissa/scripts/config.sql
+mysql -u root -p abscissa < /srv/www/website/database/config.sql
